cf0163df13c0f3ad03bb89a9ecca1bb4f0729bae
nirwan1999 <64488310+nirwan1999@users.noreply.github.com>
2021-08-15 22:42:35 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package TicTacToe;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TicTacToe tictactoe = new TicTacToe();
+
+	}
+
+}
